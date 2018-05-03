d36a162d407a7840075ee9a89f437b16e0867498
hhh <18846431340@163.com>
2018-05-03 19:08:41 +0800

    user

--- client/pages/user/logined_me/resume/other_resume/other_resume.js
+++ client/pages/user/logined_me/resume/other_resume/other_resume.js
@@ -4,7 +4,46 @@ Page({
    * 页面的初始数据
    */
   data: {
-    
+    path: '',
+    msg:"还未上传简历哦"
+  },
+  upload: function () {
+    var that = this
+    wx.chooseImage({
+      count: 1,
+      sizeType: ['original', 'compressed'],
+      sourceType: ['album', 'camera'],
+      success: function (res) {
+        var tempFilePaths = res.tempFilePaths
+        console.log(tempFilePaths)
+        wx.uploadFile({
+          url: 'http://example.weixin.qq.com/upload',
+          filePath: tempFilePaths[0],
+          name: 'file',
+          formData: {
+            'user': 'test'
+          },
+          success: function (res) {
+            var data = res.data
+            wx.showModal({
+              title: '上传文件返回状态',
+              content: '成功',
+              success: function (res) {
+                if (res.confirm) {
+                  console.log('用户点击确定')
+                }
+              }
+            })                          //do something
+          },
+          fail: function (res) {
+            console.log(res)
+          }
+        })
+        that.setData({
+          path: tempFilePaths
+        })
+      }
+    })
   },
 
   /**
