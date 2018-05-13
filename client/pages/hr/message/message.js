db4a741d74d9b1745831aa0f125676075e05f7ce
yangmiaoym <yangmiao_ym@163.com>
2018-05-13 14:51:03 +0800

    hr

--- client/pages/hr/message/message.js
+++ client/pages/hr/message/message.js
@@ -1,12 +1,65 @@
 // pages/hr/message/message.js
 Page({
-
+  data: {
+    name: '',
+    sex: '',
+    company: '',
+    email: '',
+    tel: ''
+  },
+  bindKeyInputname: function (e) {
+    this.setData({
+      name: e.detail.value
+    })
+    console.log(this.data.name);
+  },
+  bindKeyInputsex: function (e) {
+    this.setData({
+      sex: e.detail.value
+    })
+    console.log(this.data.sex);
+  },
+  bindKeyInputcompany: function (e) {
+    this.setData({
+      company: e.detail.value
+    })
+    console.log(this.data.company);
+  },
+  bindKeyInputemail: function (e) {
+    this.setData({
+      email: e.detail.value
+    })
+    console.log(this.data.email);
+  },
+  bindKeyInputtel: function (e) {
+    this.setData({
+      tel: e.detail.value
+    })
+    console.log(this.data.tel);
+  },
+  save: function () {
+    wx.request({
+      url: 'https://zfbwoz2h.qcloud.la/HR/update_hr',
+      data: {
+        name: this.data.name,
+        sex: this.data.sex,
+        company: this.data.company,
+        email: this.data.email,
+        tel: this.data.tel
+      },
+      success: function (res) {
+        console.log(res.data)
+        wx.setStorage({
+          key: "key",
+          data: res.data
+        })
+      }
+    })
+  },
   /**
    * 页面的初始数据
    */
-  data: {
-  
-  },
+
 
   /**
    * 生命周期函数--监听页面加载
