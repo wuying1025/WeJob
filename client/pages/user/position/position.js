7061d61e6ddeb78100da20ae1bfe612783c37abe
hhh <18846431340@163.com>
2018-05-20 13:27:00 +0800

    user

--- client/pages/user/position/position.js
+++ client/pages/user/position/position.js
@@ -5,28 +5,113 @@ Page({
    */
   data: {
     test:[],
-    id:''
+    id:'',
+    p_id:'',
+    src:"collect.png",
+    user_id:"",
+    c_id:""
   },
-
+  collect: function (){
+    var that = this;
+    var id = this.data.p_id;
+    console.log(id);
+    wx.getStorage({
+      key: 'aaa',
+      success: function (res) {
+        var u_id = res.data.u_id;
+        console.log(u_id);
+        if (that.data.src=="collect.png"){
+          console.log(111);
+          wx.request({
+            url: 'https://zfbwoz2h.qcloud.la/User/collect_position',
+            data: {
+              u_id: u_id,
+              p_id: id
+            },
+            success: function (res) {
+              that.setData({
+                src: "collected.png"
+              });
+              wx.showToast({
+                title: '收藏成功',
+                icon: 'success',
+                duration: 2000
+              })
+            }
+          })
+        } 
+        if(that.data.src == "collected.png"){
+          wx.request({
+            url: 'https://zfbwoz2h.qcloud.la/User/del_collect_by_u_id_p_id',
+            data: {
+              c_id: that.data.c_id
+            },
+            success: function (res) {
+              that.setData({
+                src: "collected.png"
+              });
+              wx.showToast({
+                title: '取消收藏',
+                icon: 'success',
+                duration: 2000
+              })
+            }
+          })
+        }
+       
+      }
+    });
+  },
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-    var that = this;    
+    var that = this;
     var id = options.p_id;
     console.log(id);
-    wx.request({
-      url: 'https://zfbwoz2h.qcloud.la/user/get_pos_message', //仅为示例，并非真实的接口地址
-      data: {
-        id: id
-      },
+    wx.getStorage({
+      key: 'aaa',
       success: function (res) {
-        console.log(res.data);
-        that.setData({
-          test: res.data
+        var u_id = res.data.u_id;
+        console.log(u_id);
+        wx.request({
+          url: 'https://zfbwoz2h.qcloud.la/user/get_pos_message', //仅为示例，并非真实的接口地址
+          data: {
+            id: id
+          },
+          success: function (res) {
+            console.log(res.data);
+            that.setData({
+              test: res.data,
+              p_id: id
+            });
+          }
         });
+        wx.request({
+          url: 'https://zfbwoz2h.qcloud.la/user/get_collect_by_u_id_p_id',
+          data: {
+            id: id,
+            u_id: u_id
+          },
+          success: function (res) {
+            var img = res.data;
+            console.log(img);
+            if(img>0){
+              that.setData({
+                src:"collected.png",
+                c_id:img
+              })
+            }else{
+              that.setData({
+                src: "collect.png"
+              })
+            }
+          
+          }
+        })
       }
-    })
+    });
+   
   },
 
   /**
